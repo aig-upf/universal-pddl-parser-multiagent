27d5ff8c49432168dc0491a2afb70f586141213b
Daniel Furelos Blanco <danielfb93@gmail.com>
2016-10-20 18:34:34 +0200

    add simple test for ConcurrencyDomain

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,6 +5,7 @@
 #include <minicppunit/MiniCppUnit.h>
 #include <parser/Instance.h>
 #include <multiagent/MultiagentDomain.h>
+#include <multiagent/ConcurrencyDomain.h>
 
 class MultiagentTests : public TestFixture<MultiagentTests>
 {
@@ -12,6 +13,7 @@ public:
 	TEST_FIXTURE( MultiagentTests )
 	{
 		TEST_CASE( multiagentTest );
+		TEST_CASE( concurrencyTest );
 	}
 
 	template < typename T >
@@ -33,6 +35,14 @@ public:
 
 	void multiagentTest() {
 		parser::multiagent::MultiagentDomain dom( "domains/Multilog_dom.pddl" );
+		parser::pddl::Instance ins( dom, "domains/Multilog_ins.pddl" );
+
+		checkEqual( dom, "tests/expected/Multilog_dom.pddl" );
+		checkEqual( ins, "tests/expected/Multilog_ins.pddl" );
+	}
+	
+	void concurrencyTest() {
+		parser::multiagent::ConcurrencyDomain dom( "domains/Multilog_dom.pddl" );
 		parser::pddl::Instance ins( dom, "domains/Multilog_ins.pddl" );
 
 		checkEqual( dom, "tests/expected/Multilog_dom.pddl" );
