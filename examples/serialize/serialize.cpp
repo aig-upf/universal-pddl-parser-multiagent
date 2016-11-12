141c8a2757fa0f17c5146ec55f10cb210fe62be1
Daniel Furelos Blanco <danielfb93@gmail.com>
2016-11-12 12:17:00 +0100

    added predicate creation to serialized domain

--- examples/serialize/serialize.cpp
+++ examples/serialize/serialize.cpp
@@ -1,7 +1,52 @@
 
 #include <parser/Instance.h>
+#include <multiagent/ConcurrencyDomain.h>
 
 using namespace parser::pddl;
+
+void addPredicates( Domain * d, Domain * cd ) {
+	parser::multiagent::ConcurrencyDomain * dp = dynamic_cast<parser::multiagent::ConcurrencyDomain*>( d );
+
+	for ( unsigned i = 0; i < dp->preds.size(); ++i ) {
+		if ( dp->cpreds.index( dp->preds[i]->name ) == -1 )
+		{
+			cd->createPredicate( d->preds[i]->name, d->typeList( d->preds[i] ) );
+		}
+		else
+		{
+			cd->createPredicate( "POS-" + d->preds[i]->name, d->typeList( d->preds[i] ) );
+			cd->createPredicate( "NEG-" + d->preds[i]->name, d->typeList( d->preds[i] ) );
+			cd->createPredicate( "REQ-" + d->preds[i]->name, d->typeList( d->preds[i] ) );
+		}
+	}
+}
+
+void addActions( Domain * d, Domain * cd ) {
+	/*
+	for (unsigned i = 0; i < d->actions.size(); ++i ) {
+		std::string nameStart = "START-" + d->actions[i]->name;
+		std::string nameDo = "DO-" + d->actions[i]->name;
+		std::string nameEnd = "END-" + d->actions[i]->name;
+		cd->createAction( nameStart );
+		cd->createAction( nameDo );
+		cd->createAction( nameEnd );
+		cd->addEff( 0, nameEnd, );
+	}*/
+}
+
+Domain * createClassicalDomain( Domain * d ) {
+	Domain * cd = new Domain;
+	cd->name = d->name;
+	cd->condeffects = cd->cons = cd->typed = true;
+
+	// add types and constants
+	cd->setTypes( d->copyTypes() );
+
+	addPredicates( d, cd );
+	addActions( d, cd );
+
+	return cd;
+}
 
 int main( int argc, char *argv[] ) {
 	if ( argc < 3 ) {
@@ -9,5 +54,18 @@ int main( int argc, char *argv[] ) {
 		exit(1);
 	}
 
+	// load multiagent domain and instance
+	Domain * d = new parser::multiagent::ConcurrencyDomain( argv[1] );
+	Instance * ins = new Instance( *d, argv[2] );
+
+	// create classical/single-agent domain
+	Domain * cd = createClassicalDomain( d );
+
+	cd->print(std::cout);
+
+	delete d;
+	delete ins;
+	delete cd;
+
 	return 0;
-} 
+}
