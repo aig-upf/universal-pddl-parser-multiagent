d226832daeb490422a9b780a580634c0f05c3d27
Daniel Furelos Blanco <danielfb93@gmail.com>
2017-02-07 17:44:42 +0100

    added domain and instance generators

---
+++ domains/tablemover/generate_domain.py
@@ -0,0 +1,255 @@
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: python generate_domain.py <num-tables>"
+    exit()
+
+numTables = int(sys.argv[1])
+
+domPddl = "(define (domain tablemover)\n"
+domPddl += "(:requirements :typing :conditional-effects :multi-agent)\n"
+domPddl += "(:types agent block table - locatable\n"
+for i in range(0, numTables):
+    domPddl += "\tside" + str(i) + " - side\n"
+domPddl += "\tlocatable room side\n"
+domPddl += ")\n"
+
+domPddl += "(:constants "
+for i in range(0, numTables):
+    domPddl += "Table" + str(i) + " "
+domPddl += "- table)"
+
+domPddl += """
+(:predicates
+\t(on-table ?b - block ?t - table)
+\t(on-floor ?b - block)
+\t(down ?s - side)
+\t(up ?s - side)
+\t(clear ?s - side)
+\t(at-side ?a - agent ?s - side)
+\t(lifting ?a - agent ?s - side)
+\t(inroom ?l - locatable ?r - room)
+\t(available ?a - agent)
+\t(handempty ?a - agent)
+\t(holding ?a - agent ?b - block)
+\t(connected ?r1 ?r2 - room)
+)
+"""
+
+domPddl += "(:concurrent"
+domPddl += """
+\t(pickup-floor ?a - agent ?b - block ?r - room)
+\t(putdown-floor ?a - agent ?b - block ?r - room)
+\t(pickup-table ?a - agent ?b - block ?r - room ?t - table)
+\t(leave-table ?a - agent ?s - side)
+"""
+
+for i in range(0, numTables):
+    domPddl += "\t(putdown-table-%d ?a - agent ?b - block ?r - room)\n" % (i)
+    domPddl += "\t(to-table-%d ?a - agent ?r - room ?s - side%d)\n" % (i, i)
+    domPddl += "\t(move-table-%d ?a - agent ?r1 ?r2 - room ?s - side%d)\n" % (i, i)
+    domPddl += "\t(lift-side-%d ?a - agent ?s - side%d)\n" % (i, i)
+    domPddl += "\t(lower-side-%d ?a - agent ?s - side%d)\n" % (i, i)
+domPddl += ")"
+
+domPddl += """
+(:action pickup-floor
+    :parameters (?a - agent ?b - block ?r - room)
+    :precondition (and
+                    (on-floor ?b)
+                    (inroom ?a ?r)
+                    (inroom ?b ?r)
+                    (available ?a)
+                    (handempty ?a)
+                    (forall (?a2 - agent) (not (pickup-floor ?a2 ?b ?r)))
+                )
+	:effect	(and
+                    (not (on-floor ?b))
+                    (not (inroom ?b ?r))
+                    (not (handempty ?a))
+                    (holding ?a ?b)
+            )
+)
+(:action putdown-floor
+	:parameters (?a - agent ?b - block ?r - room)
+	:precondition (and
+                        (available ?a)
+                        (inroom ?a ?r)
+                        (holding ?a ?b)
+                    )
+	:effect	(and
+                (on-floor ?b)
+                (inroom ?b ?r)
+                (handempty ?a)
+                (not (holding ?a ?b))
+            )
+)
+(:action pickup-table
+    :parameters (?a - agent ?b - block ?r - room ?t - table)
+    :precondition (and
+                    (on-table ?b ?t)
+                    (inroom ?a ?r)
+                    (inroom ?t ?r)
+                    (available ?a)
+                    (handempty ?a)
+                    (forall (?a2 - agent) (not (pickup-table ?a2 ?b ?r ?t)))
+                )
+    :effect (and
+                (not (on-table ?b ?t))
+                (not (handempty ?a))
+                (holding ?a ?b)
+            )
+)
+(:action leave-table
+    :parameters (?a - agent ?s - side)
+    :precondition (and
+                    (at-side ?a ?s)
+                    (not (lifting ?a ?s))
+                )
+    :effect	(and
+                    (clear ?s)
+                    (not (at-side ?a ?s))
+                    (available ?a)
+            )
+)
+"""
+
+for i in range(0, numTables):
+    domPddl += """
+(:action putdown-table-%d
+    :parameters (?a - agent ?b - block ?r - room)
+    :precondition (and
+                    (inroom ?a ?r)
+                    (inroom Table%d ?r)
+                    (available ?a)
+                    (holding ?a ?b)
+                    ; check table not lifted
+                    (forall (?s - side%d)
+                        (down ?s)
+                    )
+                    ; check table not intended to be lifted!
+                    (forall (?a2 - agent ?s - side%d) (not (lift-side-%d ?a2 ?s)))
+                )
+	:effect	(and
+                    (on-table ?b Table%d)
+                    (handempty ?a)
+                    (not (holding ?a ?b))
+                )
+)
+""" % (i, i, i, i, i, i)
+
+    domPddl += """
+(:action to-table-%d
+	:parameters (?a - agent ?r - room ?s - side1)
+	:precondition (and
+                    (clear ?s)
+                    (inroom ?a ?r)
+                    (inroom Table%d ?r)
+                    (available ?a)
+                    (forall (?a2 - agent) (not (to-table-%d ?a2 ?r ?s)))
+                )
+	:effect	(and
+                (not (clear ?s))
+                (at-side ?a ?s)
+                (not (available ?a))
+            )
+)
+""" % (i, i, i)
+
+    domPddl += """
+(:action move-table-%d
+    :parameters (?a - agent ?r1 ?r2 - room ?s - side%d)
+    :precondition (and
+                    (lifting ?a ?s)
+                    (inroom ?a ?r1)
+                    (connected ?r1 ?r2)
+                    (exists (?a2 - agent ?s2 - side%d)
+                        (and
+                            (not (= ?s ?s2))
+                            (move-table-%d ?a2 ?r1 ?r2 ?s2)
+                        )
+                    )
+                )
+    :effect (and
+                (not (inroom ?a ?r1))
+                (not (inroom Table%d ?r1))
+                (inroom ?a ?r2)
+                (inroom Table%d ?r2)
+            )
+)
+""" % (i, i, i, i, i, i)
+
+    domPddl += """
+(:action lift-side-%d
+    :parameters (?a - agent ?s - side%d)
+    :precondition (and
+                    (down ?s)
+                    (at-side ?a ?s)
+                    (handempty ?a)
+                    (forall (?a2 - agent ?s2 - side%d)
+                        (not (lower-side-%d ?a2 ?s2))
+                    )
+                )
+    :effect (and
+                (not (down ?s))
+                (up ?s)
+                (lifting ?a ?s)
+                (not (handempty ?a))
+                (forall (?b - block ?r - room ?s2 - side%d)
+                        (when (and
+                                (inroom Table%d ?r)
+                                (on-table ?b Table%d)
+                                (down ?s2)
+                                (forall (?a2 - agent)
+                                        (not (lift-side-%d ?a2 ?s2))
+                                )
+                            )
+                            (and
+                                (on-floor ?b)
+                                (inroom ?b ?r)
+                                (not (on-table ?b Table%d))
+                            )
+                        )
+                )
+	)
+)
+""" % (i, i, i, i, i, i, i, i, i)
+
+    domPddl += """
+(:action lower-side-%d
+    :parameters (?a - agent ?s - side%d)
+    :precondition (and
+                    (lifting ?a ?s)
+                    (forall (?a2 - agent ?s2 - side%d)
+                        (not (lift-side-%d ?a2 ?s2))
+                    )
+                )
+    :effect (and
+                (down ?s)
+                (not (up ?s))
+                (not (lifting ?a ?s))
+                (handempty ?a)
+                (forall (?b - block ?r - room ?s2 - side)
+                        (when (and
+                                (inroom Table%d ?r)
+                                (on-table ?b Table%d)
+                                (up ?s2)
+                                (forall (?a2 - agent)
+                                    (not (lower-side-%d ?a2 ?s2))
+                                )
+                            )
+                            (and
+                                (on-floor ?b)
+                                (inroom ?b ?r)
+                                (not (on-table ?b Table%d))
+                            )
+                        )
+                )
+        )
+)
+""" % (i, i, i, i, i, i, i, i)
+
+domPddl += ")"
+
+with open("tablemover_dom_" + str(numTables) + ".pddl", 'w') as f:
+    f.write(domPddl)
---
+++ domains/tablemover/generate_instance.py
@@ -0,0 +1,94 @@
+import sys
+from random import randint
+import random_connected_graph as rcg
+
+if len(sys.argv) < 7:
+	print "Usage: python generate.py <num-nodes> <num-edges> <num-blocks> <num-agents> <num-tables> <instance-number>"
+	exit()
+
+print "Generating tablemover problem..."
+
+numNodes = int(sys.argv[1])
+numEdges = int(sys.argv[2])
+numBlocks = int(sys.argv[3])
+numAgents = int(sys.argv[4])
+numTables = int(sys.argv[5])
+instanceNumber = int(sys.argv[6])
+
+print "Number of nodes:", numNodes
+print "Number of edges:", numEdges
+print "Number of blocks:", numBlocks
+print "Number of agents:", numAgents
+print "Numver of tables:", numTables
+
+nodes = [x for x in range(0, numNodes)]
+
+graph = rcg.random_walk(nodes, numEdges)
+edges = graph.edges
+
+agentsLoc = [0] * numAgents
+for i in range(0, numAgents):
+	agentsLoc[i] = randint(0, numNodes - 1)
+
+blocksLoc = [0] * numBlocks
+for i in range(0, numBlocks):
+	blocksLoc[i] = randint(0, numNodes - 1)
+
+finalLoc = randint(0, numNodes - 1)
+
+problemName = "table" + str(numNodes) + "_" + str(numEdges) + "_" + str(numBlocks)
+
+insPddl = "(define (problem " + problemName + ") (:domain tablemover)\n"
+
+insPddl += "(:objects\n"
+for i in range(0, numAgents):
+	insPddl += "\ta" + str(i) + " - agent\n"
+for i in range(0, numBlocks):
+	insPddl += "\tb" + str(i) + " - block\n"
+for i in range(0, numNodes):
+	insPddl += "\tr" + str(i) + " - room\n"
+for i in range(0, numTables):
+	insPddl += "\tleft" + str(i) + " right" + str(i) + " - side" + str(i) + "\n"
+insPddl += ")\n"
+
+insPddl += "(:init\n"
+for i in range(0, numBlocks):
+	insPddl += "\t(on-floor b" + str(i) + ")\n"
+for i in range(0, numBlocks):
+	insPddl += "\t(inroom b" + str(i) + " r" + str(blocksLoc[i]) + ")\n"
+for i in range(0, numAgents):
+	insPddl += "\t(inroom a" + str(i) + " r" + str(agentsLoc[i]) + ")\n"
+for i in range(0, numAgents):
+	insPddl += "\t(available a" + str(i) + ")\n"
+for i in range(0, numAgents):
+	insPddl += "\t(handempty a" + str(i) + ")\n"
+for i in range(0, len(edges)):
+	origin, desti = edges[i]
+	insPddl += "\t(connected r" + str(origin) + " r" + str(desti) + ")\n"
+	insPddl += "\t(connected r" + str(desti) + " r" + str(origin) + ")\n"
+for i in range(0, numTables):
+	insPddl += "\t(down left" + str(i) + ")\n"
+	insPddl += "\t(down right" + str(i) + ")\n"
+for i in range(0, numTables):
+	insPddl += "\t(clear left" + str(i) + ")\n"
+	insPddl += "\t(clear right" + str(i) + ")\n"
+for i in range(0, numTables):
+	insPddl += "\t(inroom Table" + str(i) + " r" + str(randint(0, numNodes - 1)) + ")\n"
+insPddl += ")\n"
+
+insPddl += "(:goal (and\n"
+for i in range(0, numTables):
+	insPddl += "\t(down left" + str(i) + ")\n"
+	insPddl += "\t(down right" + str(i) + ")\n"
+for i in range(0, numBlocks):
+	insPddl += "\t(on-floor b" + str(i) + ")\n"
+for i in range(0, numBlocks):
+	insPddl += "\t(inroom b" + str(i) + " r" + str(finalLoc) + ")\n"
+
+insPddl += "))\n"
+insPddl += ")"
+
+print insPddl
+
+with open(problemName + "_" + str(instanceNumber) + ".pddl", 'w') as f:
+	f.write(insPddl)
