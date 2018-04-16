95019e876957be8f69b275713d876100290991cd
Daniel Furelos Blanco <danielfb93@gmail.com>
2018-04-16 15:09:15 +0200

    test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,63 +10,80 @@
 class MultiagentTests : public TestFixture<MultiagentTests>
 {
 public:
-	TEST_FIXTURE( MultiagentTests )
-	{
-		TEST_CASE( multiagentMultilogTest );
-		TEST_CASE( multiagentTablemoverTest );
-		TEST_CASE( concurrencyTablemoverTest );
-		TEST_CASE( concurrencySimpleTablemoverTest );
-	}
+    TEST_FIXTURE( MultiagentTests )
+    {
+        TEST_CASE( multiagentMultilogTest );
+        TEST_CASE( multiagentMazeTest );
+        TEST_CASE( concurrencyMazeTest );
+        TEST_CASE( multiagentWorkshopTest );
+        TEST_CASE( concurrencyWorkshopTest );
+        TEST_CASE( concurrencyTablemoverTest );
+    }
 
-	template < typename T >
-	void checkEqual( T & prob, const std::string & file ) {
-		std::ifstream f(file.c_str());
-		if (!f) throw std::runtime_error(std::string("Failed to open file '") + file + "'");
-		std::string s, t;
-		
-		while(std::getline(f, s)){
-			t += s + "\n";
-		}
+    template < typename T >
+    void checkEqual( T & prob, const std::string & file ) {
+        std::ifstream f(file.c_str());
+        if (!f) throw std::runtime_error(std::string("Failed to open file '") + file + "'");
+        std::string s, t;
 
-		std::ostringstream ds;
-		ds << prob;
-		ASSERT_EQUALS( t, ds.str() );
-		std::ofstream of("ins.txt");
-		of<<ds.str();
-	}
+        while(std::getline(f, s)){
+            t += s + "\n";
+        }
 
-	void multiagentMultilogTest() {
-		parser::multiagent::MultiagentDomain dom( "domains/multilog/Multilog_dom.pddl" );
-		parser::pddl::Instance ins( dom, "domains/multilog/Multilog_ins.pddl" );
+        std::ostringstream ds;
+        ds << prob;
+        ASSERT_EQUALS( t, ds.str() );
+        std::ofstream of("ins.txt");
+        of<<ds.str();
+    }
 
-		checkEqual( dom, "tests/expected/multilog/Multilog_dom.pddl" );
-		checkEqual( ins, "tests/expected/multilog/Multilog_ins.pddl" );
-	}
-	
-	void multiagentTablemoverTest() {
-		parser::multiagent::MultiagentDomain dom( "domains/tablemover/Tablemover_dom_cn.pddl" );
-		parser::pddl::Instance ins( dom, "domains/tablemover/Tablemover_ins.pddl" );
+    void multiagentMultilogTest() {
+        parser::multiagent::MultiagentDomain dom( "domains/multilog/Multilog_dom.pddl" );
+        parser::pddl::Instance ins( dom, "domains/multilog/Multilog_ins.pddl" );
 
-		checkEqual( dom, "tests/expected/tablemover/Tablemover_dom_cn.pddl" );
-		checkEqual( ins, "tests/expected/tablemover/Tablemover_ins.pddl" );
-	}
-	
-	void concurrencyTablemoverTest() {
-		parser::multiagent::ConcurrencyDomain dom( "domains/tablemover/Tablemover_dom_cal.pddl" );
-		parser::pddl::Instance ins( dom, "domains/tablemover/Tablemover_ins.pddl" );
+        checkEqual( dom, "tests/expected/multilog/Multilog_dom.pddl" );
+        checkEqual( ins, "tests/expected/multilog/Multilog_ins.pddl" );
+    }
 
-		checkEqual( dom, "tests/expected/tablemover/Tablemover_dom_cal.pddl" );
-		checkEqual( ins, "tests/expected/tablemover/Tablemover_ins.pddl" );
-	}
-	
-	void concurrencySimpleTablemoverTest() {
-		parser::multiagent::ConcurrencyDomain dom( "domains/tablemover/Tablemover_simple_dom_cal.pddl" );
-		parser::pddl::Instance ins( dom, "domains/tablemover/Tablemover_simple_ins.pddl" );
+    void multiagentMazeTest() {
+        parser::multiagent::MultiagentDomain dom( "domains/maze/domain/maze_dom_cn.pddl" );
+        parser::pddl::Instance ins( dom, "domains/maze/problems/maze5_4_1.pddl" );
 
-		checkEqual( dom, "tests/expected/tablemover/Tablemover_simple_dom_cal.pddl" );
-		checkEqual( ins, "tests/expected/tablemover/Tablemover_simple_ins.pddl" );
-	}
+        checkEqual( dom, "tests/expected/maze/maze_dom_cn.pddl" );
+        checkEqual( ins, "tests/expected/maze/maze5_4_1.pddl" );
+    }
+
+    void concurrencyMazeTest() {
+        parser::multiagent::ConcurrencyDomain dom( "domains/maze/domain/maze_dom_cal.pddl" );
+        parser::pddl::Instance ins( dom, "domains/maze/problems/maze5_4_1.pddl" );
+
+        checkEqual( dom, "tests/expected/maze/maze_dom_cal.pddl" );
+        checkEqual( ins, "tests/expected/maze/maze5_4_1.pddl" );
+    }
+
+    void multiagentWorkshopTest() {
+        parser::multiagent::MultiagentDomain dom( "domains/workshop/domain/workshop_dom_cn.pddl" );
+        parser::pddl::Instance ins( dom, "domains/workshop/problems/workshop2_2_2_4.pddl" );
+
+        checkEqual( dom, "tests/expected/workshop/workshop_dom_cn.pddl" );
+        checkEqual( ins, "tests/expected/workshop/workshop2_2_2_4.pddl" );
+    }
+
+    void concurrencyWorkshopTest() {
+        parser::multiagent::ConcurrencyDomain dom( "domains/workshop/domain/workshop_dom_cal.pddl" );
+        parser::pddl::Instance ins( dom, "domains/workshop/problems/workshop2_2_2_4.pddl" );
+
+        checkEqual( dom, "tests/expected/workshop/workshop_dom_cal.pddl" );
+        checkEqual( ins, "tests/expected/workshop/workshop2_2_2_4.pddl" );
+    }
+
+    void concurrencyTablemoverTest() {
+        parser::multiagent::ConcurrencyDomain dom( "domains/tablemover/domain/table_domain1.pddl" );
+        parser::pddl::Instance ins( dom, "domains/tablemover/problems/table4_2_1.pddl" );
+
+        checkEqual( dom, "tests/expected/tablemover/table_domain1.pddl" );
+        checkEqual( ins, "tests/expected/tablemover/table4_2_1.pddl" );
+    }
 };
 
 REGISTER_FIXTURE( MultiagentTests )
-
